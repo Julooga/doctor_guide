8c1ea15f14440464bb11de42936859b24379c7a9
imjurney <jurneyx2@gmail.com>
2025-06-22 18:43:27 +0900

    fix: 시간 형식 설정을 한국어에서 영어로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     });
 
   const formatTime = (date: Date) => {
-    return date.toLocaleTimeString('ko-KR', {
+    return date.toLocaleTimeString('en', {
       hour: '2-digit',
       minute: '2-digit',
     });
